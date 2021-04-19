154d5044f84daa6e74101445baf86cc8c67df889
NoeFabris <noe@elysresidence.com>
2021-04-19 17:01:46 +0200

    Pt. 3 (Da finire)

--- myscript.js
+++ myscript.js
@@ -1,35 +1,64 @@
-var sfondo = getElemen
-var km = prompt("Distance to travel in km");
-var age = prompt("Your Age");
-console.log(km);
-console.log(age);
 
-if ((km > 0) && (age >= 0)) {
-    if (age < 18) {
-        var price = km * 0.21;
-        var price20 = (price - (price * 20 / 100)).toFixed(2);
-        console.log(price20);
-        document.getElementById('price').innerHTML= price20 ;
-    } else {
-        if (age < 65) {
+window.addEventListener('load', function(){
+    var mainForm = document.getElementById('mainForm')
+
+
+    mainForm.addEventListener('submit', onSubmit)
+})
+
+var fullName = document.getElementById('user-name')
+var km
+var ageGroup
+
+function onSubmit(event){
+    event.preventDefault()
+    // recupera il form che ha causato il submit
+    var form = event.currentTarget
+
+    // recupera gli es dal form 
+    var elements = form.elements
+
+    fullName += elements.fullName.value
+    km = elements.km.value
+    ageGroup = elements.ageGroup.value
+    
+
+
+    console.log(elements)
+    console.log('nome = ' + fullName)
+    console.log('km = ' + km)
+    console.log('Fascia d/eta = ' + ageGroup)
+
+    if ((km > 0) && (ageGroup >= 0)) {
+        if (ageGroup == 'Minorenne') {
             var price = km * 0.21;
-            console.log(price);
-            document.getElementById('price').innerHTML= price ;
+            var price20 = (price - (price * 20 / 100)).toFixed(2);
+            console.log(price20);
+            // document.getElementById('price').innerHTML= price20 ;
         } else {
-            var price = km * 0.21;
-            var price40 = (price - (price * 40 / 100)).toFixed(2);
-            console.log(price40);
-            document.getElementById('price').innerHTML= price40 ;
+            if (ageGroup == 'Maggiorenne') {
+                var price = km * 0.21;
+                console.log(price);
+                // document.getElementById('price').innerHTML= price ;
+            } else {
+                var price = km * 0.21;
+                var price40 = (price - (price * 40 / 100)).toFixed(2);
+                console.log(price40);
+                // document.getElementById('price').innerHTML= price40 ;
+            }
+        }
+    } else {
+        if (isNaN(km)) {
+            // document.getElementById('price').innerHTML= "Please insert KM in numbers" ;
         }
     }
-} else {
-    if (isNaN(km) && isNaN(age)) {
-        document.getElementById('price').innerHTML= "Please use numbers" ;
-    } else if (isNaN(age)) {
-        document.getElementById('price').innerHTML= "Please insert AGE in numbers" ;
-    } else {
-        document.getElementById('price').innerHTML= "Please insert KM in numbers" ;
-    }
+
 }
+
+// console.log('nome = ' + fullName)
+// console.log('km = ' + km)
+// console.log('Fascia d/eta = ' + ageGroup)
+
+
 
 
